885745c13c6a257b65e9d673fce35bee1105f51e
TheKissay <thekissay112@gmail.com>
2021-10-20 13:10:07 +0200

    Initial commit

---
+++ src/main/java/com/nttdata/bootcamp/saludoIdioma/SaludoIdiomaApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp.saludoIdioma;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class SaludoIdiomaApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SaludoIdiomaApplication.class, args);
+		
+		
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/saludoIdioma/SaludoIdiomaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.nttdata.bootcamp.saludoIdioma;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SaludoIdiomaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
